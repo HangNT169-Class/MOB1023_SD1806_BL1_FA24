7f0589fb906be9b031aa785c14c3d90209ef9a68
HangNT <HangNT169@fpt.edu.vn>
2024-09-08 21:20:06 +0700

    Init project

---
+++ src/main/MOB1023_SD1806_BL1_FA24.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author hangnt
+ */
+public class MOB1023_SD1806_BL1_FA24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
